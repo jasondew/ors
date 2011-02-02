c2190808ca82c1dbcf4c195310c3e93e6cc4fb66
asceth <machinist@asceth.com>
2011-02-02 11:15:48 -0500

    console command

--- lib/ors/commands/console.rb
+++ lib/ors/commands/console.rb
@@ -1,5 +1,16 @@
-module ORS
-  def console
+module ORS::Commands
+  class Console < Base
+    def run
+      # set pretending to true to get back command then exec it since
+      # we don't want this to execute remotely
+      ORS::Config.pretending = true
 
+      exec remote_execute(console_server,
+                          %(cd #{deploy_directory}),
+                          %(rails console #{environment}))
+    end
+
+    def help
+    end
   end
 end
