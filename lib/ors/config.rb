26791a2effdaed9c74a01d7c98abb1a8bcd0dbae
asceth <machinist@asceth.com>
2011-02-02 09:34:39 -0500

    config as a module included into utils

--- lib/ors/config.rb
+++ lib/ors/config.rb
@@ -1,45 +1,43 @@
 module ORS
-  class Config
-    class << self
-      def gateway
-        "deploy-gateway"
-      end
+  module Config
+    def gateway
+      "deploy-gateway"
+    end
 
-      def deploy_user
-        "deployer"
-      end
+    def deploy_user
+      "deployer"
+    end
 
-      def repo
-        "ors_git"
-      end
+    def repo
+      "ors_git"
+    end
 
-      def base_path
-        "/var/www"
-      end
+    def base_path
+      "/var/www"
+    end
 
-      def web_servers
-        %w(koala)
-      end
+    def web_servers
+      %w(koala)
+    end
 
-      def app_servers
-        %w(eel jellyfish squid)
-      end
+    def app_servers
+      %w(eel jellyfish squid)
+    end
 
-      def migration_server
-        "tuna"
-      end
+    def migration_server
+      "tuna"
+    end
 
-      def console_server
-        "tuna"
-      end
+    def console_server
+      "tuna"
+    end
 
-      def ruby_servers
-        app_servers + [migration_server]
-      end
+    def ruby_servers
+      app_servers + [migration_server]
+    end
 
-      def all_servers
-        web_servers + app_servers + [migration_server]
-      end
+    def all_servers
+      web_servers + app_servers + [migration_server]
     end
   end
 end
