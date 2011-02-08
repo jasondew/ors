e2cc63e0104b28ec2d033d64627001ab7e3d090e
Jason Dew <jason.dew@gmail.com>
2011-02-08 08:28:09 -0500

    fixing typo

--- lib/ors/helpers.rb
+++ lib/ors/helpers.rb
@@ -8,7 +8,7 @@ module ORS
 
       execute_command server, %(cd #{base_path}),
                              %(rm -rf #{deploy_directory}),
-                             %(git clone #{REPO}:#{name} #{deploy_directory}),
+                             %(git clone #{repo}:#{name} #{deploy_directory}),
                              %(mkdir -p #{deploy_directory}/tmp/pids),
                              %(mkdir -p #{deploy_directory}/log)
     end
