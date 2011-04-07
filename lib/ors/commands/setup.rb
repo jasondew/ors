0987e70c6fc8edbdd8021ba3e20aa26ffbf0bf74
John Long <machinist@asceth.com>
2011-04-07 17:05:02 -0400

    setup magic

--- lib/ors/commands/setup.rb
+++ lib/ors/commands/setup.rb
@@ -5,12 +5,17 @@ module ORS::Commands
     def execute
       info "setting up #{name} #{environment}..."
 
-      execute_in_parallel(all_servers) {|server| setup_repo server }
-      execute_in_parallel(ruby_servers) {|server| setup_ruby server }
+      info "Are you sure? ('crashandburn' + ctrl+D^2)"
+      if STDIN.read == "crashandburn"
+        execute_in_parallel(all_servers) {|server| setup_repo server }
+        execute_in_parallel(ruby_servers) {|server| setup_ruby server }
 
-      execute_command migration_server, %(source ~/.rvm/scripts/rvm),
-                                        %(cd #{deploy_directory}),
-                                        %(RAILS_ENV=#{environment} bundle exec rake db:create)
+        execute_command migration_server, %(source ~/.rvm/scripts/rvm),
+                                          %(cd #{deploy_directory}),
+                                          %(RAILS_ENV=#{environment} bundle exec rake db:create)
+      else
+        info "Stopping crash and burn setup"
+      end
     end
 
   end
