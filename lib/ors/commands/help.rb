0162c7398c97f613760029f0303eda71bd38c405
Philip Hallstrom <philip@pjkh.com>
2011-04-11 13:15:32 -0700

    sort the help output commands alphabetically

--- lib/ors/commands/help.rb
+++ lib/ors/commands/help.rb
@@ -7,17 +7,17 @@ module ORS::Commands
 Usage: ./ors <action> [environment=production] [options]
 
 === Actions
-help          You're looking at it
+changes       View changes between what is deployed and committed
 console       Bring up a console on the production servers
-logs          Show the last few log entries from the production servers
 deploy        Update the code, run the migrations, and restart unicorn
-setup         Sets up the default environment on the servers
-update        Updates the code on all servers
+help          You're looking at it
+logs          Show the last few log entries from the production servers
 migrate       Runs the migrations on the migration server
+restart       Retarts unicorn on the app servers
+setup         Sets up the default environment on the servers
 start         Starts up unicorn on the app servers
 stop          Stops unicorn on the app servers
-restart       Retarts unicorn on the app servers
-changes       View changes between what is deployed and committed
+update        Updates the code on all servers
 
 === Environments
 Must be one of: production demo staging
