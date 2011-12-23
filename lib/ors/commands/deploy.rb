13257193cbeef486dfb2323f4f49b531432e6580
Jason Dew <jason.dew@gmail.com>
2011-12-22 22:22:42 -0500

    run remote deploy hook before restarting

--- lib/ors/commands/deploy.rb
+++ lib/ors/commands/deploy.rb
@@ -5,15 +5,17 @@ module ORS::Commands
     def execute
       info "deploying #{name} #{environment}..."
 
-      [Update, Migrate, Restart].each {|command| run command }
-
-      eval ERB.new(deploy_hook).result(binding) if deploy_hook
+      [Update, Migrate].each {|command| run command }
 
       if remote_deploy_hook
         execute_in_parallel(app_servers) do |server|
           execute_command server, prepare_environment, "RAILS_ENV=#{environment} #{remote_deploy_hook}"
         end
       end
+
+      run Restart
+
+      eval ERB.new(deploy_hook).result(binding) if deploy_hook
     end
 
   end
