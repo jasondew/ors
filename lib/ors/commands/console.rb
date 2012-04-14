b521073e05cfe960d7d869a8773f61edb5715fdd
John Long <machinist@asceth.com>
2012-04-14 03:04:28 -0700

    make sure we use rvm with console command

--- lib/ors/commands/console.rb
+++ lib/ors/commands/console.rb
@@ -3,7 +3,7 @@ class ORS
     class Console < Base
       def execute
         execute_command(ORS.config[:console_server],
-                        prepare_environment,
+                        prepare_environment_with_rvm,
                         %(if [ -f script/rails ]; then bundle exec rails console #{ORS.config[:environment]}; else ./script/console #{ORS.config[:environment]}; fi),
                         :exec => true)
       end
